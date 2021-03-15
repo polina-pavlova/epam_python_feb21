f6aaec06d9a1ac8603cb292cd8469bcd5ed36956
polina-pavlova <pollypavlova98@gmail.com>
2021-03-16 00:41:53 +0300

    Fixed task4

--- homework2/tasks/hw4.py
+++ homework2/tasks/hw4.py
@@ -25,9 +25,15 @@ def cache(func: Callable) -> Callable:
     cached_results = {}
 
     def func_cache(*args, **kwargs):
-        if args in cached_results:
+        if args:
+            if args in cached_results:
+                return cached_results[args]
+            cached_results[args] = func(*args)
             return cached_results[args]
-        cached_results[args] = func(*args)
-        return cached_results[args]
+        if kwargs:
+            if kwargs in cached_results:
+                return cached_results[kwargs]
+            cached_results[kwargs] = func(**kwargs)
+            return cached_results[kwargs]
 
     return func_cache
