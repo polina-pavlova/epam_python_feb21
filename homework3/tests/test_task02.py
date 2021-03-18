51b1f9e8060c1d525eec46041d396679bf062a33
polina-pavlova <pollypavlova98@gmail.com>
2021-03-19 02:52:09 +0300

    Uncomment test2

--- homework3/tests/test_task02.py
+++ homework3/tests/test_task02.py
@@ -1,7 +1,7 @@
-# import pytest
-#
-# from homework3.task02.task02 import parallel_calculation
-#
-#
-# def test_sum_for_range_0_501_slow_calculation():
-#     assert parallel_calculation() == 1025932
+import pytest
+
+from homework3.task02.task02 import parallel_calculation
+
+
+def test_sum_for_range_0_501_slow_calculation():
+    assert parallel_calculation() == 1025932
