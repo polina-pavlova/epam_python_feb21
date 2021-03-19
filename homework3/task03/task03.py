879a615cc887eba296a5d4fc5fdd7298a307fe24
polina-pavlova <pollypavlova98@gmail.com>
2021-03-19 20:52:15 +0300

    Delete sample_data from task_03

--- homework3/task03/task03.py
+++ homework3/task03/task03.py
@@ -28,20 +28,3 @@ def make_filter(**keywords):
 
     filter_funcs.append(keyword_filter_func)
     return Filter(filter_funcs)
-
-
-sample_data = [
-    {
-        "name": "Bill",
-        "last_name": "Gilbert",
-        "occupation": "was here",
-        "type": "person",
-    },
-    {"is_dead": True, "kind": "parrot", "type": "bird", "name": "polly"},
-]
-
-print(
-    make_filter(name="polly", type="bird").apply(sample_data)
-)  # should return only second entry from the list
-print(make_filter(name="polly", type="bird", last_name="test").apply(sample_data))
-# There are multiple bugs in this code. Find them all and write tests for faulty cases.
