752e4f40494a0acccd933514d961522f8a25f76e
polina-pavlova <pollypavlova98@gmail.com>
2021-04-01 23:39:23 +0300

    Added homework 7

---
+++ homework7/tasks/hw2.py
@@ -0,0 +1,25 @@
+"""
+Given two strings. Return if they are equal when both are typed into
+empty text editors. # means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+Examples:
+    Input: s = "ab#c", t = "ad#c"
+    Output: True
+    # Both s and t become "ac".
+
+    Input: s = "a##c", t = "#a#c"
+    Output: True
+    Explanation: Both s and t become "c".
+
+    Input: a = "a#c", t = "b"
+    Output: False
+    Explanation: s becomes "c" while t becomes "b".
+
+"""
+import re
+
+
+def backspace_compare(first: str, second: str):
+    return re.sub(r".{0,1}#", "", first) == re.sub(r".{0,1}#", "", second)
---
+++ homework7/tests/test_hw2.py
@@ -0,0 +1,11 @@
+import pytest
+
+from homework7.tasks.hw2 import backspace_compare
+
+
+@pytest.mark.parametrize(
+    ["first", "second", "expected_result"],
+    [("ab#c", "ad#c", True), ("a##c", "#a#c", True), ("a#c", "b", False)],
+)
+def test_backspace_compare(first, second, expected_result):
+    assert backspace_compare(first, second) == expected_result
